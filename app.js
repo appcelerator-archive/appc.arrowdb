bd9f413e40b6618b3fb3a53f3422fc93a86b8a41
Dawson Toth <dtoth@appcelerator.com>
2014-12-16 13:42:44 -0500

    https://jira.appcelerator.org/browse/API-230

--- app.js
+++ app.js
@@ -1,44 +1,9 @@
 /**
- * NOTE: this file is simply for testing your Connector and
- * not used or packaged with the actual connector when published
+ * NOTE: This file is simply for testing this connector and will not
+ * be used or packaged with the actual connector when published.
  */
 var APIBuilder = require('appcelerator').apibuilder,
 	server = new APIBuilder();
-
-// lifecycle examples
-server.on('starting', function() {
-	server.logger.info('server is starting!');
-});
-
-server.on('started', function() {
-	server.logger.info('server started!');
-});
-
-//--------------------- implement authorization ---------------------//
-
-// fetch our configured apikey
-var apikey = server.get('apikey');
-server.logger.info('APIKey is:', apikey);
-
-function APIKeyAuthorization(req, resp, next) {
-	if (!apikey) {
-		return next();
-	}
-	if (req.headers['apikey']) {
-		var key = req.headers['apikey'];
-		if (key == apikey) {
-			return next();
-		}
-	}
-	resp.status(401);
-	return resp.json({
-		id: "com.appcelerator.api.unauthorized",
-		message: "Unauthorized",
-		url: ""
-	});
-}
-
-//--------------------- simple user model ---------------------//
 
 var User = APIBuilder.Model.extend('user', {
 	fields: {
@@ -50,14 +15,8 @@ var User = APIBuilder.Model.extend('user', {
 		password: { type: String, hidden: true },
 		password_confirmation: { type: String, hidden: true }
 	},
-	connector: 'appc.acs'	// a model level connector
+	connector: 'appc.acs'
 });
-
-// add an authorization policy for all requests at the server log
-server.authorization = APIKeyAuthorization;
-
-// create a user api from a user model
 server.addModel(User);
 
-// start the server
 server.start();
