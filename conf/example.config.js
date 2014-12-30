d17582c14800184d5d1200613ff4a716d9618ac3
Dawson Toth <dtoth@appcelerator.com>
2014-12-30 15:07:05 -0500

    defaultConfig

---
+++ conf/example.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	connectors: {
+		'appc.acs': {
+			key: '', // Place your ACS APP Key from https://cloud.appcelerator.com/apps/ here.
+			username: '', // A user from https://cloud.appcelerator.com/your_app_id/users to use for looking up metadata.
+			password: '' // The password for your user.
+		}
+	}
+};
