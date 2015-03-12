2d6106ca8a1c8a73422fab637ad10d795a7bc06b
Dawson Toth <dtoth@appcelerator.com>
2015-03-12 12:16:33 -0400

    [API-478] Don't Point at Website
    https://jira.appcelerator.org/browse/API-478

--- conf/example.config.js
+++ conf/example.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	connectors: {
 		'appc.arrowdb': {
 			key: '', // Place your ArrowDB App Key
-			username: '', // A user from https://cloud.appcelerator.com/your_app_id/users to use for looking up metadata.
+			username: '', // A user from your ArrowDB to use for looking up metadata.
 			password: '', // The password for your user.
 
 			generateModelsFromSchema: true // Generate models for the built in ArrowDB types (ACLs, Users, etc).
