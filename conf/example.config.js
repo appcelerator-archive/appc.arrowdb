581bf1900facac343a8f1e8f8896b9b4e214ee88
Dawson Toth <dtoth@appcelerator.com>
2015-04-01 13:25:55 -0400

    [API-30] ArrowDB Per-Session Logins
    https://jira.appcelerator.org/browse/API-30

--- conf/example.config.js
+++ conf/example.config.js
@@ -1,7 +1,14 @@
 module.exports = {
 	connectors: {
 		'appc.arrowdb': {
-			key: '', // Place your ArrowDB App Key
+			/*
+			 requireSessionLogin allows you to configure whether or not anonymous requests can use your default
+			 account (specified below) for connecting to ArrowDB. Set this to "true" to require requests to specify
+			 their own credentials or their own session token (via the headers user, and pass, or sessionCookieString).
+			 */
+			requireSessionLogin: false,
+
+			key: '', // Your ArrowDB App Key.
 			username: '', // A user from your ArrowDB to use for looking up metadata.
 			password: '', // The password for your user.
 
