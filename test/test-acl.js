51181b8f61dde69fcd5cb208fb2afd0239941f4b
Dawson Toth <dtoth@appcelerator.com>
2015-08-12 12:54:55 -0400

    [API-931] Add "show" API Methods
    We exposed the method programmatically before, but we didn't describe it for the REST API to consume. Now we do, and we test it to keep it that way.
    
    https://jira.appcelerator.org/browse/API-931

--- test/test-acl.js
+++ test/test-acl.js
@@ -12,15 +12,49 @@ var tests = require('./_base.tests'),
 describe('ACL', function () {
 
 	var modelName = 'appc.arrowdb/acl',
+		aclName = 'my.acl.' + Date.now(),
+		auth,
+		server,
 		Model;
 
 	init(this, function () {
+		auth = {
+			user: this.server.config.apikey,
+			password: ''
+		};
 		Model = this.connector.getModel(modelName);
+		server = this.server;
 	});
 
 	describe('Create', function () {
 		tests.create(modelName, {
-			name: 'my.acl.' + Date.now()
+			name: aclName
+		});
+
+		it('should allow calling show', function (next) {
+			this.connector.getModel(modelName).show({
+				name: aclName
+			}, function (error, response) {
+				assert.ifError(error);
+				should(response).have.property('name', aclName);
+				should(response).have.property('user_id');
+				next();
+			});
+		});
+
+		it('should expose a "show" api', function makeSureAuthIsRequired(cb) {
+			request({
+				method: 'GET',
+				uri: 'http://localhost:' + server.port + '/api/' + modelName + '/show?name=' + aclName,
+				auth: auth,
+				json: true
+			}, function (err, response, body) {
+				should(body.success).be.true;
+				should(body).have.property('acl');
+				should(body.acl).have.property('name', aclName);
+				should(body.acl).have.property('user_id');
+				cb();
+			});
 		});
 	});
 
@@ -33,7 +67,7 @@ describe('ACL', function () {
 	});
 
 	describe('Update', function () {
-		tests.update(modelName, {name: 'my.acl.' + Date.now()});
+		tests.update(modelName, {name: aclName});
 	});
 
 	describe('Delete All', function () {
