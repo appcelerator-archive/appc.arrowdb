2ed71ec1c384e155e818170ab86e1c79e7e20153
Dawson Toth <dtoth@appcelerator.com>
2015-10-27 17:24:10 -0400

    [API-1106] Add Failing Test for Update over REST

--- test/test-user.js
+++ test/test-user.js
@@ -4,7 +4,8 @@
 require('./_base');
 var tests = require('./_base.tests');
 
-var assert = require('assert'),
+var Arrow = require('arrow'),
+	assert = require('assert'),
 	async = require('async'),
 	should = require('should'),
 	request = require('request');
@@ -12,20 +13,16 @@ var assert = require('assert'),
 describe('User', function () {
 
 	var modelName = 'appc.arrowdb/user',
-		Model;
+		Model,
+		server;
 
 	init(this, function () {
 		Model = this.connector.getModel(modelName);
+		server = this.server;
 	});
 
 	describe('Create', function () {
-		tests.create(modelName, {
-			username: 'test.user.' + Date.now(),
-			password: 'test.password',
-			password_confirmation: 'test.password',
-			first_name: 'Test',
-			last_name: 'User'
-		});
+		tests.create(modelName, getCreationDict('create'));
 	});
 
 	describe('FindAll and FindOne', function () {
@@ -40,4 +37,67 @@ describe('User', function () {
 		tests.update(modelName, {first_name: 'Dawson', last_name: 'Toth'});
 	});
 
+	describe('Extension', function () {
+		it('should update users properly over REST', function (done) {
+			var urlToHit = 'http://localhost:' + server.port + '/api/' + modelName,
+				auth = {
+					user: server.config.apikey,
+					password: ''
+				};
+
+			Model.create([
+				getCreationDict('extend1'),
+				getCreationDict('extend2'),
+				getCreationDict('extend3')
+			], function (err, results) {
+				assert.ifError(err);
+				should(results).be.an.Object;
+				should(results.length).be.within(0, 10);
+
+				async.each(results,
+					function updateEachResult(result, next) {
+						var json = result.toJSON();
+						json.first_name = 'Updated:' + result.id;
+						request({
+							method: 'PUT',
+							uri: urlToHit + '/' + result.id,
+							auth: auth,
+							json: json
+						}, function (err, response, body) {
+							assert.ifError(err);
+							next();
+						});
+					},
+					checkUpdates);
+
+				function checkUpdates() {
+					async.each(results,
+						function checkEachResult(result, next) {
+							request({
+								method: 'GET',
+								uri: urlToHit + '/' + result.id,
+								auth: auth,
+								json: true
+							}, function (err, response, body) {
+								assert.ifError(err);
+								should(body[body.key]).have.property('first_name', 'Updated:' + result.id);
+								next();
+							});
+						},
+						done);
+				}
+			});
+		});
+	});
+
+	function getCreationDict(suffix) {
+		return {
+			email: 'test.' + Date.now() + '.' + suffix + '@appc.com',
+			username: 'test.' + Date.now() + '.' + suffix,
+			password: 'test.password',
+			password_confirmation: 'test.password',
+			first_name: 'Test',
+			last_name: 'User'
+		};
+	}
 });
